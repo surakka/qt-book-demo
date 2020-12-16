3ffb3d607db85d30eb72e9e2b0d58c4a010a8150
Sami Surakka <sami.surakka@IPM-005.home>
2020-12-16 08:29:51 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,107 @@
+module.exports = {
+  title: 'Qt for MCUs',
+  tagline: 'The tagline of my site',
+  url: 'https://qt-book-demo.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+  organizationName: 'surakka', // Usually your GitHub org/user name.
+  projectName: 'qt-book-demo', // Usually your repo name.
+  stylesheets: [
+    'https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600;700&display=swap',
+  ],
+  themeConfig: {
+    prism: {
+      additionalLanguages: ['qml'],
+    },
+    navbar: { 
+      title: 'QtBook',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/qt',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/qt',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/qtproject',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/qt',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
